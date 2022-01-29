891f530c5dbe9647073f462d30c6d16da6bf2906
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-29 18:02:35 -0300

    Update data

---
+++ src/utils/data/categoryData.js
@@ -0,0 +1,18 @@
+export const categoryData = [
+  {
+    id: '1',
+    title: 'Harina Trigo',
+  },
+  {
+    id: '2',
+    title: 'Harina Maiz',
+  },
+  {
+    id: '3',
+    title: 'Harina Arroz',
+  },
+  {
+    id: '4',
+    title: 'Sin Tacc',
+  },
+];
---
+++ src/utils/data/productsList.js
@@ -0,0 +1,63 @@
+export const productsList = [
+  {
+    id: 1,
+    title: 'Pan Saborizado',
+    category: '4',
+    precio: 350,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYaffBNe3ttKLNPkGv4MVTW5Eq4Do53q_G5g&usqp=CAU'
+  },
+  {
+    id: 2,
+    title: 'Budines',
+    category: '1',
+    precio: 800
+  },
+  {
+    id: 3,
+    title: 'Tortas',
+    category: '2',
+    precio: 1500
+  },
+  {
+    id: 4,
+    title: 'Medialunas',
+    category: '3',
+    precio: 120
+  },
+  {
+    id: 5,
+    title: 'Galletas',
+    category: '4',
+    precio: 80
+  },
+  {
+    id: 6,
+    title: 'Masas finas',
+    category: '1',
+    precio: 200
+  },
+  {
+    id: 7,
+    title: 'Scons',
+    category: '3',
+    precio: 150
+  },
+  {
+    id: 8,
+    title: 'Pan de Campo',
+    category: '2',
+    precio: 440
+  },
+  {
+    id: 9,
+    title: 'Churros',
+    category: '3',
+    precio: 120
+  },
+  {
+    id: 10,
+    title: 'Grisines',
+    category: '4',
+    precio: 50
+  },
+];
