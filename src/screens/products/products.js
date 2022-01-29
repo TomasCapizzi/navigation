c5880b2663d04766aaedecc022d44e58f2425bd9
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-29 18:02:09 -0300

    Update products.js

--- src/screens/products/products.js
+++ src/screens/products/products.js
@@ -1,16 +1,34 @@
-import {Button, SafeAreaView, Text, View} from 'react-native';
+import {FlatList, SafeAreaView, View} from 'react-native';
 
+import ProductItem from '../../components/ProductItem/ProductItem';
 import React from 'react';
+import {productsList} from '../../utils/data/productsList';
 import styles from './styles';
 
-export default function products({navigation}) {
+export default function products({navigation, route}) {
+  const list = productsList.filter(
+    product => product.category === route.params.categoryId, //navigation.getParam('categoryId')
+  );
+
+  const handleSelectedProduct = item => {
+    navigation.navigate('detail', {
+      productId: item.id,
+      name: item.title,
+      product: item
+    });
+  };
+
+  const renderProduct = ({item}) => {
+    return <ProductItem item={item} onSelect={handleSelectedProduct}/>;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>Products pa</Text>
-        <Button
-          title="See Detail"
-          onPress={() => navigation.navigate('detail')}
+        <FlatList
+          data={list}
+          renderItem={renderProduct}
+          keyExtractor={item => item.id}
         />
       </View>
     </SafeAreaView>
