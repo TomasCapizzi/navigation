92432e642c1e8bd633084ae89232bb9d4755fb05
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-29 18:02:42 -0300

    Fake cart data

---
+++ src/utils/data/cartFake.js
@@ -0,0 +1,16 @@
+export const cartFake = [
+  {
+    id: '1',
+    title: 'Pan Saborizado',
+    category: '4',
+    precio: 350,
+    cantidad: 3,
+  },
+  {
+    id: '7',
+    title: 'Scons',
+    category: '3',
+    precio: 150,
+    cantidad: 6,
+  },
+];
