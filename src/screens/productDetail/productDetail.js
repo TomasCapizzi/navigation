f031b8440a0ac8e682ee955c428a52330d3ae854
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-31 14:20:18 -0300

    Update productDetail.js
    Redux

--- src/screens/productDetail/productDetail.js
+++ src/screens/productDetail/productDetail.js
@@ -1,14 +1,25 @@
-import {Button, Image, SafeAreaView, Text, View} from 'react-native';
+import {Button, SafeAreaView, Text, TextInput, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 
 import React from 'react';
+import {addCart} from '../../store/actions/cart.action';
 import styles from './styles';
 
-const productDetail = ({navigation, route}) => {
+const productDetail = ({navigation}) => {
+  const dispatch = useDispatch();
+  const product = useSelector(state => state.products.selected);
+
+  const handleCart = item => {
+    dispatch(addCart(item));
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>{route.params.name}</Text>
-        <Text>${route.params.product.precio}</Text>
+        <Text>{product.title}</Text>
+        <Text>${product.precio}</Text>
+        <TextInput/>
+        <Button title="Comprar" onPress={() => handleCart(product)} />
         <Button
           title="Categorias"
           onPress={() => navigation.navigate('categories')}
